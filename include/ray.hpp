a9493006fdf55f92c0881142019a3a624f3bdc83
B33Boy <abhip202@gmail.com>
2025-07-16 17:58:25 -0400

    feat: completed ray class

---
+++ include/ray.hpp
@@ -0,0 +1,28 @@
+#ifndef RAY_HPP
+#define RAY_HPP
+
+#include "vec3.hpp"
+
+class ray
+{
+public:
+    // ============= Constructors =============
+    constexpr ray() noexcept = default;
+    constexpr ray(point3 const &origin, vec3 const &dir) noexcept : origin_(origin), dir_(dir) {}
+
+    // ============= Getters =============
+    [[nodiscard]] constexpr point3 const &origin() const noexcept { return origin_; }
+    [[nodiscard]] constexpr vec3 const &direction() const noexcept { return dir_; }
+
+    // ============= Member functions =============
+    [[nodiscard]] constexpr point3 at(double t) const noexcept
+    {
+        return origin_ + t * dir_;
+    }
+
+private:
+    point3 origin_{};
+    vec3 dir_{};
+};
+
+#endif // RAY_HPP
