022d7580cc1e2de49209f68343baccc59a18200d
B33Boy <abhip202@gmail.com>
2025-07-16 14:50:58 -0400

    feat: write color from vec3

---
+++ include/color.hpp
@@ -0,0 +1,27 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+#include "vec3.hpp"
+
+#include <ostream>
+#include <algorithm> // std::clamp
+#include <cmath>     // std::round
+
+using color = vec3;
+
+void write_color(std::ostream &out, color const &pixel_color)
+{
+    // rgb values should be within [0, 1]
+    auto r = std::clamp(pixel_color.x(), 0.0, 1.0);
+    auto g = std::clamp(pixel_color.y(), 0.0, 1.0);
+    auto b = std::clamp(pixel_color.z(), 0.0, 1.0);
+
+    // convert to the range [0, 255]
+    int rbyte = static_cast<int>(std::round(255 * r));
+    int gbyte = static_cast<int>(std::round(255 * g));
+    int bbyte = static_cast<int>(std::round(255 * b));
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
