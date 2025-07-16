8e201082947560ff299cb9f5d6c95c72f4825dcc
B33Boy <abhip202@gmail.com>
2025-07-16 15:17:04 -0400

    feat: demo ppm with vec3 and color

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "color.hpp"
+#include "vec3.hpp"
 
-int main() {
-    std::cout << "Hello, Raytracer!" << std::endl;
-    return 0;
+int main()
+{
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height
+              << "\n255\n";
+
+    for (int j = 0; j < image_height; j++)
+    {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++)
+        {
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
+        }
+        std::cout << '\n';
+    }
+
+    std::clog << "\rDone.                 \n";
 }
