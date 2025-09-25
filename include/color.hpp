631c0c0642a7d13ea67e86dccdf782fe34a7b6b6
B33Boy <abhip202@gmail.com>
2025-09-25 12:34:00 -0400

    fix: revert to original write_color

--- include/color.hpp
+++ include/color.hpp
@@ -1,7 +1,6 @@
 #ifndef COLOR_HPP
 #define COLOR_HPP
 
-#include "interval.hpp"
 #include "vec3.hpp"
 
 #include <algorithm> // std::clamp
@@ -17,11 +16,10 @@ void write_color(std::ostream& out, color const& pixel_color)
     auto g = std::clamp(pixel_color.y(), 0.0, 1.0);
     auto b = std::clamp(pixel_color.z(), 0.0, 1.0);
 
-    // Translate the [0,1] component values to the byte range [0,255].
-    static const interval intensity(0.000, 0.999);
-    int rbyte = int(256 * intensity.clamp(r));
-    int gbyte = int(256 * intensity.clamp(g));
-    int bbyte = int(256 * intensity.clamp(b));
+    // convert to the range [0, 255]
+    int rbyte = static_cast<int>(std::round(255 * r));
+    int gbyte = static_cast<int>(std::round(255 * g));
+    int bbyte = static_cast<int>(std::round(255 * b));
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
