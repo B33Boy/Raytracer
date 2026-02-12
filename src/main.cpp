dfbf3f75312b151e3bfbda7961c68dfb4adc1873
B33Boy <abhip202@gmail.com>
2026-02-12 00:25:46 -0500

    feat: updated image

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,13 @@ int main()
     // ====== World ======
     hittable_list world;
 
-    auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = std::make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_ground = std::make_shared<metal>(color(0.8, 0.8, 0.0));
+    auto material_center = std::make_shared<metal>(color(0.1, 0.2, 0.5));
+    auto material_left = std::make_shared<lambertian>(color(0.8, 0.145, 0.329));
+    auto material_right = std::make_shared<lambertian>(color(0.278, 0.839, 0.153));
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.70), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
